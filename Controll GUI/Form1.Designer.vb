1ea41a7d6d34dff7fa16a3fd548fe4e95845b6fe
henry1758f <henry1758f@gmail.com>
2015-12-20 23:49:33 +0800

    20151220 publish

---
+++ Controll GUI/Form1.Designer.vb
@@ -0,0 +1,395 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.設定SettingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.串列ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.關於AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.StatusStrip2 = New System.Windows.Forms.StatusStrip()
+        Me.ComboBox_Portselect = New System.Windows.Forms.ComboBox()
+        Me.ComboBox_Baudrate = New System.Windows.Forms.ComboBox()
+        Me.ComboBox_Parity = New System.Windows.Forms.ComboBox()
+        Me.ComboBox_Stopbits = New System.Windows.Forms.ComboBox()
+        Me.ComboBox_Databit = New System.Windows.Forms.ComboBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(17, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 100)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(132, 10)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 100)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(248, 10)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 100)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(17, 116)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 100)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(132, 116)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 100)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(248, 116)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 100)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(17, 222)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 100)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Button7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(132, 222)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 100)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "Button8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(248, 222)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 100)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "Button9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button5)
+        Me.Panel1.Controls.Add(Me.Button9)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Button8)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button7)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button6)
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Location = New System.Drawing.Point(6, 24)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(368, 335)
+        Me.Panel1.TabIndex = 9
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Panel1)
+        Me.GroupBox1.Location = New System.Drawing.Point(229, 96)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(393, 389)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "車體控制"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RichTextBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(628, 96)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(354, 364)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "資料接收"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(6, 25)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(324, 316)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.設定SettingToolStripMenuItem, Me.關於AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(994, 27)
+        Me.MenuStrip1.TabIndex = 13
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        '設定SettingToolStripMenuItem
+        '
+        Me.設定SettingToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.串列ToolStripMenuItem})
+        Me.設定SettingToolStripMenuItem.Name = "設定SettingToolStripMenuItem"
+        Me.設定SettingToolStripMenuItem.Size = New System.Drawing.Size(105, 23)
+        Me.設定SettingToolStripMenuItem.Text = "設定 Setting"
+        '
+        '串列ToolStripMenuItem
+        '
+        Me.串列ToolStripMenuItem.Name = "串列ToolStripMenuItem"
+        Me.串列ToolStripMenuItem.Size = New System.Drawing.Size(289, 26)
+        Me.串列ToolStripMenuItem.Text = "串列埠設定 Serial Port Setting"
+        '
+        '關於AboutToolStripMenuItem
+        '
+        Me.關於AboutToolStripMenuItem.Name = "關於AboutToolStripMenuItem"
+        Me.關於AboutToolStripMenuItem.Size = New System.Drawing.Size(97, 23)
+        Me.關於AboutToolStripMenuItem.Text = "關於 About"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 498)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(994, 22)
+        Me.StatusStrip1.TabIndex = 14
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'StatusStrip2
+        '
+        Me.StatusStrip2.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.StatusStrip2.Location = New System.Drawing.Point(0, 476)
+        Me.StatusStrip2.Name = "StatusStrip2"
+        Me.StatusStrip2.Size = New System.Drawing.Size(994, 22)
+        Me.StatusStrip2.TabIndex = 15
+        Me.StatusStrip2.Text = "StatusStrip2"
+        '
+        'ComboBox_Portselect
+        '
+        Me.ComboBox_Portselect.FormattingEnabled = True
+        Me.ComboBox_Portselect.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.ComboBox_Portselect.Location = New System.Drawing.Point(20, 60)
+        Me.ComboBox_Portselect.Name = "ComboBox_Portselect"
+        Me.ComboBox_Portselect.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox_Portselect.TabIndex = 16
+        '
+        'ComboBox_Baudrate
+        '
+        Me.ComboBox_Baudrate.FormattingEnabled = True
+        Me.ComboBox_Baudrate.Location = New System.Drawing.Point(20, 112)
+        Me.ComboBox_Baudrate.Name = "ComboBox_Baudrate"
+        Me.ComboBox_Baudrate.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox_Baudrate.TabIndex = 17
+        '
+        'ComboBox_Parity
+        '
+        Me.ComboBox_Parity.FormattingEnabled = True
+        Me.ComboBox_Parity.Location = New System.Drawing.Point(20, 180)
+        Me.ComboBox_Parity.Name = "ComboBox_Parity"
+        Me.ComboBox_Parity.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox_Parity.TabIndex = 18
+        '
+        'ComboBox_Stopbits
+        '
+        Me.ComboBox_Stopbits.FormattingEnabled = True
+        Me.ComboBox_Stopbits.Location = New System.Drawing.Point(20, 231)
+        Me.ComboBox_Stopbits.Name = "ComboBox_Stopbits"
+        Me.ComboBox_Stopbits.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox_Stopbits.TabIndex = 19
+        '
+        'ComboBox_Databit
+        '
+        Me.ComboBox_Databit.FormattingEnabled = True
+        Me.ComboBox_Databit.Location = New System.Drawing.Point(20, 287)
+        Me.ComboBox_Databit.Name = "ComboBox_Databit"
+        Me.ComboBox_Databit.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox_Databit.TabIndex = 20
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Controls.Add(Me.ComboBox_Parity)
+        Me.GroupBox3.Controls.Add(Me.ComboBox_Databit)
+        Me.GroupBox3.Controls.Add(Me.ComboBox_Portselect)
+        Me.GroupBox3.Controls.Add(Me.ComboBox_Stopbits)
+        Me.GroupBox3.Controls.Add(Me.ComboBox_Baudrate)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 96)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(158, 364)
+        Me.GroupBox3.TabIndex = 21
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "UART設定"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(20, 269)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(67, 15)
+        Me.Label5.TabIndex = 25
+        Me.Label5.Text = "資料位元"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 213)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 15)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "停止位"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 150)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 15)
+        Me.Label3.TabIndex = 23
+        Me.Label3.Text = "校驗方式"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 15)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "Baud Rate選擇"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 15)
+        Me.Label1.TabIndex = 21
+        Me.Label1.Text = "COM通訊埠"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(994, 520)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.StatusStrip2)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents StatusStrip2 As StatusStrip
+    Friend WithEvents 設定SettingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 串列ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 關於AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ComboBox_Portselect As ComboBox
+    Friend WithEvents ComboBox_Baudrate As ComboBox
+    Friend WithEvents ComboBox_Parity As ComboBox
+    Friend WithEvents ComboBox_Stopbits As ComboBox
+    Friend WithEvents ComboBox_Databit As ComboBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Controll GUI/Form1.vb
@@ -0,0 +1,102 @@
+﻿Imports System.IO.Ports
+
+Public Class Form1
+    Dim ReceiveStr As String = ""
+    Dim TransmitStr As String = ""
+
+    Sub Form1_initial()
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComboBox_Portselect.Items.Add(sp)
+        Next
+        'Baudrate list
+        ComboBox_Baudrate.Items.Add("2400")
+        ComboBox_Baudrate.Items.Add("4800")
+        ComboBox_Baudrate.Items.Add("9600")
+        ComboBox_Baudrate.Items.Add("14400")
+        ComboBox_Baudrate.Items.Add("19200")
+        ComboBox_Baudrate.Items.Add("28800")
+        ComboBox_Baudrate.Items.Add("38400")
+        ComboBox_Baudrate.Items.Add("57600")
+        ComboBox_Baudrate.Items.Add("76800")
+        ComboBox_Baudrate.Items.Add("115200")
+        ComboBox_Baudrate.Items.Add("Auto")
+        'Parity list
+        ComboBox_Parity.Items.Add("None 無")
+        ComboBox_Parity.Items.Add("Odd 奇同位")
+        ComboBox_Parity.Items.Add("Even 偶同位")
+        'Siopbit list
+        ComboBox_Stopbits.Items.Add("0")
+        ComboBox_Stopbits.Items.Add("1")
+        ComboBox_Stopbits.Items.Add("2")
+        'Databit List
+        ComboBox_Databit.Items.Add("8 bits")
+        'Initial select
+
+
+
+    End Sub
+
+
+    Sub GetSerialPortNames()
+        ' Show all available COM ports.
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComboBox_Portselect.Items.Add(sp)
+        Next
+
+        Dim mySerialPort As New SerialPort("COM3")
+
+        mySerialPort.BaudRate = 9600
+        mySerialPort.Parity = Parity.None
+        mySerialPort.StopBits = StopBits.One
+        mySerialPort.DataBits = 8
+        mySerialPort.Handshake = Handshake.None
+        mySerialPort.RtsEnable = True
+
+    End Sub
+
+    Sub SendSerialData(ByVal data As String)
+        ' Send strings to a serial port.
+        Using com3 As IO.Ports.SerialPort =
+            My.Computer.Ports.OpenSerialPort("COM3")
+            com3.WriteLine(data)
+        End Using
+    End Sub
+
+    Function ReceiveSerialData(ByVal port As String) As String
+        ' Receive strings from a serial port.
+        Dim returnStr As String = ""
+
+        Dim com As IO.Ports.SerialPort = Nothing
+        Try
+            com = My.Computer.Ports.OpenSerialPort(port)
+            com.ReadTimeout = 10000
+            Do
+                Dim Incoming As String = com.ReadLine()
+                If Incoming Is Nothing Then
+                    Exit Do
+                Else
+                    returnStr &= Incoming & vbCrLf
+                End If
+            Loop
+        Catch ex As TimeoutException
+            returnStr = "Error: Serial Port read timed out."
+        Finally
+            If com IsNot Nothing Then com.Close()
+        End Try
+
+        Return returnStr
+    End Function
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Form1_initial()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SendSerialData("Button1")
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
